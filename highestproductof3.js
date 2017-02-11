1c6f3d6b37f04774e41e2bc52a08daea985798d2
Autumn Smith <autumn07@gmail.com>
2017-02-11 13:51:07 -0800

    attempt at greedy solution

--- highestproductof3.js
+++ highestproductof3.js
@@ -2,7 +2,11 @@
 // you can get from three of the integers.
 // The input arrayOfInts will always have at least three integers.
 
-const highestProductof3 = (array) => {
+const highestProductOfThree= (array) => {
+  //base case not 3
+  if (array.length < 3 ) {
+    return null;
+  }
   //base case of only 3
   if (array.length === 3 ) {
     return array.reduce((n,m) => (n*m));
@@ -10,7 +14,37 @@ const highestProductof3 = (array) => {
   //could sort but would take O(n lg n) time
   //can do O(n) using greedy approach
 
+  let productOf2 = {
+    lowest: null,
+    highest: null,
+  };
+
+  let highestProductOf3 = null;
+  let lowestNum = null;
+  let highestNum = null;
+
+  for (let i = 1, length = array.length; i < length; i++) {
+    if (array[i] < lowestNum || lowestNum === null) {
+      lowestNum = array[i];
+    }
+    if (array[i] > highestNum || highestNum === null) {
+      highestNum = array[i];
+    }
+    if (i >= 2) {
+      let currProductOf2 = array[i-1] * array[i];
+      if (currProductOf2 > productOf2.highest || productOf2.highest === null){
+        productOf2.highest = currProductOf2;
+      }
+      if (currProductOf2 < productOf2.lowest || productOf2.lowest === null){
+        productOf2.lowest = currProductOf2;
+      }
+
+      // TODO: finish greedy algorithm
+
+
+    }
+  }
 
 };
 
-console.log(highestProductof3([2,3,4])) //should be 12;
+console.log(highestProductofThree([2,3,4])); //should be 12;
