01448233c0344e81b7e55b82a7697ec5f3ac86c9
Autumn Smith <autumn07@gmail.com>
2017-02-11 13:51:42 -0800

    greedy approach

--- productofallothernums.js
+++ productofallothernums.js
@@ -19,6 +19,7 @@ const input = [1, 7, 3, 4];
 const input2 = [1, 1, 1, 1, 1];
 const input3 = [1, 2, 0, 1, 1];
 
+// naive solution
 const productOfAllIntsExpectAtIndex = (array) => {
   let result = [];
   for (var i = 0; i < array.length; i++) {
@@ -32,10 +33,29 @@ const productOfAllIntsExpectAtIndex = (array) => {
   return result;
 };
 
+// better solution using a greedy approach
+const productOfAllIntsExpectAtIndex2 = (array) => {
+  let nextProduct = 1;
+  let prevProduct = 1;
+  let result = [];
+  for (var i = 0; i < array.length; i++) {
+    if (i !== 0){
+      prevProduct *= array[i-1];
+      result.push(prevProduct);
+    } else {
+      result.push(prevProduct);
+    }
+  }
+  return result;
+};
+
+console.log(productOfAllIntsExpectAtIndex2([1, 7, 3, 4]));
+
 const assert = (condition, expected, errorMessage) => {
   return condition === expected ? console.log('Test passed') : console.log(errorMessage);
 };
 
-assert(JSON.stringify(productOfAllIntsExpectAtIndex(input)), JSON.stringify([84, 12, 28, 21]), 'Should multiple all except current index');
-assert(JSON.stringify(productOfAllIntsExpectAtIndex(input2)), JSON.stringify([1, 1, 1, 1, 1]), 'Should multiple all except current index');
-assert(JSON.stringify(productOfAllIntsExpectAtIndex(input3)), JSON.stringify([0, 0, 2, 0, 0]), 'Should multiple all except current index');
+// naive solution tests
+// assert(JSON.stringify(productOfAllIntsExpectAtIndex(input)), JSON.stringify([84, 12, 28, 21]), 'Should multiple all except current index');
+// assert(JSON.stringify(productOfAllIntsExpectAtIndex(input2)), JSON.stringify([1, 1, 1, 1, 1]), 'Should multiple all except current index');
+// assert(JSON.stringify(productOfAllIntsExpectAtIndex(input3)), JSON.stringify([0, 0, 2, 0, 0]), 'Should multiple all except current index');
